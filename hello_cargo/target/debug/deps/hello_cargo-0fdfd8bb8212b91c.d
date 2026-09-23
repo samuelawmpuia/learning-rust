61dee6bf2374a41fa5b16b21d09f75d80cd3a469
samuelawmpuia <samuellalawmpuia1@gmail.com>
2026-09-23 22:44:25 +0530

    Remove tracking on target (i think)

--- hello_cargo/target/debug/deps/hello_cargo-0fdfd8bb8212b91c.d
+++
@@ -1,5 +0,0 @@
-/home/samuel/Documents/learnRust/learnRust/hello_cargo/target/debug/deps/hello_cargo-0fdfd8bb8212b91c.d: src/main.rs
-
-/home/samuel/Documents/learnRust/learnRust/hello_cargo/target/debug/deps/hello_cargo-0fdfd8bb8212b91c: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/samuel/Documents/learnRust/learnRust/hello_cargo/target/debug/hello_cargo: /home/samuel/Documents/learnRust/learnRust/hello_cargo/src/main.rs
